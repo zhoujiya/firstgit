5bed3cb2be9a3fa3af8d63f755d5910248e21ca4
zhouji <373441787@qq.com>
2019-05-16 20:16:58 +0800

    第一次提交git

---
+++ day01/src/cn/itcast/day01/demo01.java
@@ -0,0 +1,14 @@
+package cn.itcast.day01;
+
+public class demo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("laji");
+		
+		System.out.println("didi");
+		
+	}
+
+}
